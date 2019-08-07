72a247027648bd3f4764422799e4a48e738b73a7
Halah <halah.alshaikhly@gmail.com>
2019-08-07 12:19:03 -0700

    adds import for PushwooshFCMHelper + adds instance of intercome push client

--- src/android/FirebaseInstanceIdRouterService.java
+++ src/android/FirebaseInstanceIdRouterService.java
@@ -2,6 +2,7 @@ package com.zenput.push;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import io.intercom.android.sdk.push.IntercomPushClient;
+import com.pushwoosh.PushwooshFcmHelper;
 
 public class FirebaseInstanceIdRouterService extends FirebaseMessagingService {
 
@@ -13,6 +14,7 @@ public class FirebaseInstanceIdRouterService extends FirebaseMessagingService {
     }
 
     private void sendTokenToAnotherService(String token) {
+        IntercomPushClient intercomPushClient = new IntercomPushClient();
         intercomPushClient.sendTokenToIntercom(getApplication(), token);
     }
 }
